e2c18000eb16365cd23242f7b0a650350699a1e9
unknown <kostya.mkhalkov@bk.ru>
2024-09-24 14:18:00 +0300

    init: start project

---
+++ src/components/TodoEnter/TodoEnter.js
@@ -0,0 +1,14 @@
+
+import './TodoEnter.scss';
+
+const TodoEnter = () => {
+    return (
+        <div className='todo'>
+            <div className="todo_enter">
+                
+            </div>
+        </div>
+    )
+}
+
+export default TodoEnter;
