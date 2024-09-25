f17ec281fe2949c9664dc4ab12e3d6166d71fd14
unknown <kostya.mkhalkov@bk.ru>
2024-09-25 13:14:45 +0300

    feat: complete login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,10 @@
+import {useState} from 'react';
 
 import './Login.scss';
 
 const Login = () => {
+    const [slide, setSlide] = useState(false);
+
     return (
         <form className="login">
             <label htmlFor="login"
@@ -18,10 +21,20 @@ const Login = () => {
             <input type="password"
                    name='password' 
                    className='input_styling'/>
-            <div className="slider">
-                <div className="slider_elem"></div>
-                <span className='slider_text'>YES</span>    
-            </div>       
+            <div className={`slider ${slide ? 'slider_agree' : 'slider_disagree'}`}
+                 onClick={() => setSlide(slide => !slide)}>
+                <div className={`slider_elem ${slide ? 'slider_elem_agree' : 'slider_elem_disagree'}`}></div>
+                <span className={`slider_text ${slide ? 'slider_text_agree' : 'slider_text_disagree'}`}>{slide ? 'YES' : 'NO'}</span>  
+            </div>
+            <div className='slider_title'
+                 onClick={() => setSlide(slide => !slide)}>
+                    Keep me signed in
+            </div> 
+            <button type='submit'
+                    className='login_btn'>
+                SIGN IN
+            </button>
+            <div className='login_line'></div>        
         </form>
     )
 }
